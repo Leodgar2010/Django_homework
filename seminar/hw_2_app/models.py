e721a89866ee0cbc009499161cc35b8237b9188f
Leodgar2010 <leodgar2010@yandex.ru>
2023-08-29 22:33:40 +0300

    Third homework

--- seminar/hw_2_app/models.py
+++ seminar/hw_2_app/models.py
@@ -3,26 +3,27 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone = models.IntegerField
+    phone = models.IntegerField (default=None)
     address = models.TextField()
-    reg_date = models.DateField(auto_now_add=True)
+    reg_date = models.DateField()
 
     def __str__(self):
-       return f'{self.name}, phone: {self.surname}'
+       return f'{self.name}, phone: {self.phone}'
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     amount = models.IntegerField()
-    add_date = models.DateField(auto_now_add=True)
+    add_date = models.DateField()
 
     def __str__(self):
-       return f'{self.name}, price: {self.price}'
+       return f'{self.title}, price: {self.price}'
 
 class Order (models.Model):
     customer = models.ForeignKey(Client, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
-    date_ordered = models.DateTimeField(auto_now_add=True)
+    date_ordered = models.DateField(auto_now_add=True)
+
     def __str__(self):
-       return f'{self.customer} заказал {self.products}'
+        return f'{self.customer} заказал {self.products.all}'
