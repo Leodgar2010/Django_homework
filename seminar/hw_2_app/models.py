eb96118614a639e7a363f9483c4994d9ee27e87b
Leodgar2010 <leodgar2010@yandex.ru>
2023-09-01 13:32:00 +0300

    Forth homework

--- seminar/hw_2_app/models.py
+++ seminar/hw_2_app/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     amount = models.IntegerField()
     add_date = models.DateField()
-
+    image = models.ImageField(upload_to='images')
     def __str__(self):
        return f'{self.title}, price: {self.price}'
 
